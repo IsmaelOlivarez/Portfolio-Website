b3dbde9cf00439219e8a978524e4144c1ab87c55
Ismael Olivarez <ismael.olivarez05@gmail.com>
2025-01-04 17:39:54 -0500

    Routine commit

--- portfolio-website-2024/src/app/components/HeroSection.tsx
+++ portfolio-website-2024/src/app/components/HeroSection.tsx
@@ -13,17 +13,25 @@ export const HeroSection = () => {
             lg:grid-cols-12 gap-[32] m-[96]">
             
             <div className="col-span-12 h-[1340px] w-auto relative
-                bg-[rgba(255,255,255,0.25)] rounded-[42.1] 
+                bg-[rgb(36,36,36)] rounded-[42.1] 
                 lg:h-[670] lg:w-[1320px] mx-auto">
 
                 <section className='grid lg:grid-cols-12 lg:gap-[32] lg:mb-[45] lg:ml-[64] lg:mt-[64] lg:mr-[32]'>
 
-                    <div className='col-span-4 bg-[#5E548E] h-[123px] w-[375] rounded-[40]'>
-                        Hi! I am
+                      <div className='col-span-4 bg-transparent h-[123px] w-[375] rounded-[40]'>
+                          <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500 text-4xl lg:text-[72px] font-sans font-bold'>
+                              Hello, I'm
+                          </span>
+                      </div>
+
+                    <div className='col-span-8 bg-red-500 h-[116] mr-[32] rounded-[40]'>
+                        <h1 className='text-[128px] text-white -mt-[64] font-sans'>Ismael</h1>
                     </div>
 
-                    <div className='col-span-8 bg-red-500 h-[267] mr-[32] rounded-[40]'>
-                        Ismael
+                    <div className='col-span-4 bg-transparent h-[133.5] mr-[32] rounded-[40]'>
+                    </div>
+
+                    <div className='col-span-8 bg-red-500 h-[116] mr-[32] rounded-[40]'>
                     </div>
                 </section>
                 
@@ -32,28 +40,46 @@ export const HeroSection = () => {
 
                     <div className='col-span-4 bg-black h-[375] w-[375] rounded-full'>
                     </div>
+                    
 
-                    <div className='col-span-5 bg-green-500 h-[240] rounded-[40] relative top-[135]'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud asd.
+                    <div className='col-span-5 bg-green-500 h-[240] rounded-[40] relative top-[135]
+                        '>
+                    
+                        <h1 className='text-[#ADB7BE] text-lg lg:text-xl'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud asd.
+                        </h1>
+                    
                     </div>
 
                     <section className='col-span-3 space-y-[12px]'>
-                        <div className='bg-yellow-500 h-[72] relative top-[135] mr-[32] rounded-full'>
-                            <button>Hire Me!</button>
-                        </div>
-                        <div className='bg-yellow-500 h-[72] relative top-[135] mr-[32] rounded-full'>
-                            <button>Resume!</button>
-                        </div>
-                        <div className='bg-yellow-500 h-[72] relative top-[135] mr-[32] rounded-full'>
-                            <button>Projects!</button>
-                        </div>
-                    </section>
+
+                          <div className='h-[72] relative top-[135] mr-[32] rounded-full'>
+
+                              <button className='h-[72] w-full rounded-full bg-gradient-to-br from-purple-500 to-[#A23962] hover:bg-slate-200 text-white text-xl'>
+                                  Hire Me
+                              </button>
+
+                          </div>
+
+                          <div className='h-[72] relative top-[135] mr-[32] rounded-full'>
+
+                              <button className='h-[72] w-full rounded-full bg-gradient-to-br from-purple-500 to-[#A23962] hover:bg-slate-200 text-white text-xl'>
+                                  Projects
+                              </button>
+
+                          </div>
+
+                          <div className=' h-[72] relative top-[135] mr-[32] rounded-full'>
+                              <button className='px-1 py-1 w-full rounded-full bg-gradient-to-br from-blue-500 to-pink-500 hover:bg-slate-800
+                                 text-white text-xl'>
+                                  <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-4'>Download CV</span>
+                              </button>
+                          </div>
+
+                      </section>
 
                     
-
-
-
                 </section>
 
                 
